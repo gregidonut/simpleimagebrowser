d20bf6a076b3ca74471c742a6822a83542d2138b
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-22 04:15:02 +0800

    feat: reimplement the logic for presenting the image thumbnail its container
    this implementation reveals a public DoLoadImages() function to that can
    draw the images in a separate goroutine

--- loading/loadimage.go
+++ loading/loadimage.go
@@ -3,6 +3,10 @@ package loading
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/storage"
+	"github.com/nfnt/resize"
+	"image"
+	"log"
 )
 
 // LoadImage will add the img and to the background channel queue
@@ -13,4 +17,42 @@ func LoadImage(u fyne.URI) fyne.CanvasObject {
 	// adding the image to the background channel queue
 	loads <- bgImageLoad{u, img}
 	return img
+}
+
+// doLoadImage is responsible for reading the image file from the fyne.URI and returning
+// the in-memory image to the calling container/CanvasObject
+func doLoadImage(u fyne.URI, img *canvas.Image) {
+	read, err := storage.Reader(u)
+	if err != nil {
+		log.Println("Error opening image", err)
+		return
+	}
+	defer read.Close()
+
+	raw, _, err := image.Decode(read)
+	if err != nil {
+		log.Println("Error decoding image", err)
+		return
+	}
+
+	img.Image = scaleImage(raw)
+	img.Refresh()
+}
+
+// scaleImage is essentially a wrapper for the resize.Thumbnail function but specifically
+// uses the resize.Lanczos3 for a more efficient scaling routine
+func scaleImage(img image.Image) image.Image {
+	// specifying fixed size twice the size of the actual parent container for a more
+	// pixel-dense output (should probably adjust this to become smarter in the future)
+	return resize.Thumbnail(320, 320, img, resize.Lanczos3)
+}
+
+// DoLoadImages is the function that will be called in main as a goroutine, it will
+// range through the loads channel and call the main logic function: doLoadImage on each load
+// which will redraw the image everytime the parent container is resized since doLoadImage calls
+// the Refresh() method from the *canvas.Image struct
+func DoLoadImages() {
+	for load := range loads {
+		doLoadImage(load.uri, load.img)
+	}
 }
