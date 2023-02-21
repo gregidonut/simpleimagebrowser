f5c67194bd7c967277fd306d8531513f2b529c40
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-21 16:14:11 +0800

    feat: create an image grid container
    this container will be what is shown in app window's main space the
    default thumbnail presentation is selected

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"image/color"
+	"strconv"
 )
 
 func main() {
@@ -13,9 +14,9 @@ func main() {
 
 	w := a.NewWindow("simpleimagebrowser")
 
-	w.SetContent(makeImageItem())
-	w.Resize(fyne.NewSize(600, 600))
-	w.SetFixedSize(true)
+	w.SetContent(makeImageGrid())
+	//w.Resize(fyne.NewSize(600, 600))
+	//w.SetFixedSize(true)
 
 	w.ShowAndRun()
 }
@@ -24,10 +25,26 @@ func main() {
 // then returns a border layout container where at the bottom the label is placed and the
 // image which will consume the remaining space(which in this case will be the top since
 // only the bottom of the layout is specified to be used)
-func makeImageItem() fyne.CanvasObject {
-	label := canvas.NewText("label", color.NRGBA{R: 0xf7, G: 0xff, B: 0x5e, A: 0xff})
+func makeImageItem(imgLabel string) fyne.CanvasObject {
+	label := canvas.NewText(imgLabel, color.NRGBA{R: 0xf7, G: 0xff, B: 0x5e, A: 0xff})
 	label.Alignment = fyne.TextAlignCenter
 
 	img := canvas.NewRectangle(color.NRGBA{R: 0x8b, G: 0xff, B: 0xfe, A: 0xff})
 	return container.NewBorder(nil, label, nil, nil, img)
+}
+
+// makeImageGrid creates an image grid container using the GridWrap layout with a images
+// laid out in it to simulate a thumbnail style presentation of a list of images calling
+// the makeImageItem to present the images with a fixed cell size
+func makeImageGrid() fyne.CanvasObject {
+	items := make([]fyne.CanvasObject, 0)
+
+	// loop through images to show as thumbnails
+	for i := 1; i <= 10; i++ {
+		mockImageName := strconv.Itoa(i)
+		items = append(items, makeImageItem(mockImageName))
+	}
+
+	cellSize := fyne.NewSize(160, 160)
+	return container.NewGridWrap(cellSize, items...)
 }
