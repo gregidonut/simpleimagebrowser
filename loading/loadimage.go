2dd2e63d8620144d5ae960aeca223f348028c1d8
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-22 00:56:50 +0800

    feat: extract the logic to display the image form loading.LoadImage()
    this function's responsibility is now adding the image to the channel
    queue

--- loading/loadimage.go
+++ loading/loadimage.go
@@ -3,27 +3,14 @@ package loading
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/storage"
-	"image/color"
-	"log"
 )
 
-// LoadImage is ultimately a wrapper for the canvas.NewImageFromResource that is used to
-// display the image from the URI in it's container
+// LoadImage will add the img and to the background channel queue
 func LoadImage(u fyne.URI) fyne.CanvasObject {
-	read, err := storage.Reader(u)
-	if err != nil {
-		log.Println("Error opening image", err)
-		return canvas.NewRectangle(color.NRGBA{R: 0xff, G: 0xaf, B: 0x62, A: 0xff})
-	}
-
-	res, err := storage.LoadResourceFromURI(read.URI())
-	if err != nil {
-		log.Println("Error reading image", err)
-		return canvas.NewRectangle(color.NRGBA{R: 0xff, G: 0xaf, B: 0x62, A: 0xff})
-	}
-
-	img := canvas.NewImageFromResource(res)
+	img := canvas.NewImageFromResource(nil)
 	img.FillMode = canvas.ImageFillContain
+
+	// adding the image to the background channel queue
+	loads <- bgImageLoad{u, img}
 	return img
 }
