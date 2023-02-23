f30d697b79e4f3b8715e2445998f7e79c60d9898
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-23 17:51:56 +0800

    feat: extract worker-pools pattern from main.go
    in preparation to bring this logic into the calling function in MakeUI

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"github.com/gregidonut/simpleimagebrowser/pkg/listing"
-	"github.com/gregidonut/simpleimagebrowser/pkg/loading"
 	"github.com/gregidonut/simpleimagebrowser/pkg/ui"
-	"runtime"
 )
 
 func main() {
@@ -17,10 +15,5 @@ func main() {
 	//w.Resize(fyne.NewSize(600, 600))
 	//w.SetFixedSize(true)
 
-	var workers int // sanity checking to make sure workers is an int(1000% not necessary)
-	workers = runtime.NumCPU() / 2
-	for i := 0; i < workers; i++ {
-		go loading.DoLoadImages()
-	}
 	w.ShowAndRun()
 }
