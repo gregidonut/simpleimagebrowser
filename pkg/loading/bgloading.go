9c74586aa8030a49cf4ed16f9b73975fd41b95d2
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-23 18:19:40 +0800

    feat: define a new type loading.LoadedImage struct
    will be useful for a results channel to complete the worker-pools
    concurrency pattern

--- pkg/loading/bgloading.go
+++ pkg/loading/bgloading.go
@@ -3,10 +3,20 @@ package loading
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"image"
 )
 
 // BgImageLoad represents the image that will load in the background
 type BgImageLoad struct {
 	uri fyne.URI
 	img *canvas.Image
+}
+
+// LoadedImage represents the components of the image item from the layout
+// to actually present the image thumbnail. these two items need be fused
+// together to actually load the item which will be the responsibility of the
+// for loop reading from the results channel of the corresponding worker pool
+type LoadedImage struct {
+	decoded image.Image
+	img     *canvas.Image
 }
